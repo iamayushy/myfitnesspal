0ffeef67517317b7d5bf37f0692b2518b4425ced
@NitinMaharshi <nitin77n@gmail.com>
2022-04-06 22:56:10 +0530

    add ONE month trail part

--- src/Components/Premium.jsx
+++ src/Components/Premium.jsx
@@ -1,7 +1,146 @@
 import React from "react";
+import styles from "./Premium.module.jsx";
 
 const Premium = () => {
-  return <div></div>;
+  return (
+    <>
+      <main className={styles.jss52}>
+        <div
+          className={`${styles.MuiContainer_root} ${styles.jss53} ${styles.MuiContainer_maxWidthLg}`}
+        >
+          <div className={styles.jss54}>
+            <div className={styles.jss55}>
+              <div className={styles.jss58}>
+                <div className={styles.jss65}>Premium</div>
+              </div>
+              <div className={styles.jss59}>
+                <h1
+                  className={`${styles.MuiTypography_root} ${styles.jss74} ${styles.jss60}`}
+                >
+                  Start Your Free Trial Today
+                </h1>
+              </div>
+              <div className={styles.jss61}>
+                <p
+                  className={`${styles.MuiTypography_root} ${styles.jss62} ${styles.MuiTypography_body1}`}
+                >
+                  Premium members are 65% more likely to reach their weight loss
+                  goal.
+                </p>
+              </div>
+              <button
+                className={`${styles.MuiButtonBase_root} ${styles.MuiButton_root} ${styles.MuiButton_contained} ${styles.jss83} ${styles.jss84} ${styles.jss85} ${styles.jss63} ${styles.MuiButton_containedPrimary}`}
+                tabindex="0"
+                type="button"
+              >
+                <span className={styles.MuiButton_label}>
+                  <span
+                    className={`${styles.MuiTypography_root} ${styles.jss80}`}
+                  >
+                    Start free 1-month trial
+                  </span>
+                </span>
+              </button>
+            </div>
+            <div className={styles.jss56}>
+              <img
+                src="https://www.myfitnesspal.com/react-static/e4537e1d014a66cb6083eb6025813bb6.png"
+                className={styles.jss57}
+                alt="hero gif"
+              />
+            </div>
+          </div>
+          <div>
+            <div
+              className={`${styles.MuiGrid_root} ${styles.jss64} ${styles.MuiGrid_container} ${styles.MuiGrid_spacing_xs_3}`}
+            >
+              <div
+                className={`${styles.MuiGrid_root} ${styles.MuiGrid_item} ${styles.MuiGrid_grid_xs_12} ${styles.MuiGrid_grid_sm_4}`}
+              >
+                <div className={styles.jss86}>
+                  <div className={styles.jss87}>
+                    <img
+                      src="https://www.myfitnesspal.com/react-static/51b6c01dec78ddfb6d693b1ff8d23d44.png"
+                      className={styles.jss88}
+                      alt="feature logo"
+                    />
+                  </div>
+                  <div>
+                    <div className={styles.jss90}>
+                      <h3
+                        className={`${styles.MuiTypography_root} ${styles.jss91} ${styles.MuiTypography_h3}`}
+                      >
+                        Meal Plans &amp; Recipes
+                      </h3>
+                    </div>
+                    <p
+                      className={`${styles.MuiTypography_root} ${styles.jss92} ${styles.MuiTypography_body1}`}
+                    >
+                      Gain insight into your nutrition
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div
+                className={`${styles.MuiGrid_root} ${styles.MuiGrid_item} ${styles.MuiGrid_grid_xs_12} ${styles.MuiGrid_grid_sm_4}`}
+              >
+                <div className={styles.jss86}>
+                  <div className={styles.jss87}>
+                    <img
+                      src="https://www.myfitnesspal.com/react-static/c51db6a037b248c10816974faac845f9.png"
+                      className={styles.jss88}
+                      alt="feature logo"
+                    />
+                  </div>
+                  <div>
+                    <div className={styles.jss90}>
+                      <h3
+                        className={`${styles.MuiTypography_root} ${styles.jss91} ${styles.MuiTypography_h3}`}
+                      >
+                        Personalized Goals
+                      </h3>
+                    </div>
+                    <p
+                      className={`${styles.MuiTypography_root} ${styles.jss92} ${styles.MuiTypography_body1}`}
+                    >
+                      Get guidance &amp; coaching tailored to you
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div
+                className={`${styles.MuiGrid_root} ${styles.MuiGrid_item} ${styles.MuiGrid_grid_xs_12} ${styles.MuiGrid_grid_sm_4}`}
+              >
+                <div className={styles.jss86}>
+                  <div className={styles.jss87}>
+                    <img
+                      src="https://www.myfitnesspal.com/react-static/93c7966575adb77a179235b991596985.png"
+                      className={styles.jss88}
+                      alt="feature logo"
+                    />
+                  </div>
+                  <div>
+                    <div className={styles.jss90}>
+                      <h3
+                        className={`${styles.MuiTypography_root} ${styles.jss91} ${styles.MuiTypography_h3}`}
+                      >
+                        Ad-Free Experience
+                      </h3>
+                    </div>
+                    <p
+                      className={`${styles.MuiTypography_root} ${styles.jss92} ${styles.MuiTypography_body1}`}
+                    >
+                      Focus on your goals without distraction
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Premium;
